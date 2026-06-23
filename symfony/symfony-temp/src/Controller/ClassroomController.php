de2ad34cbd3687f6c5457b6a09879c3bb958c4e6
MylostyvyiArtem <ipz243_mao@student.ztu.edu.ua>
2026-06-23 16:18:38 +0300

    Add Symfony Classroom GET endpoint

--- symfony/symfony-temp/src/Controller/ClassroomController.php
+++ symfony/symfony-temp/src/Controller/ClassroomController.php
@@ -1,1 +1,24 @@
 <?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ClassroomController extends AbstractController
+{
+    private string $file = __DIR__ . '/../../data/classrooms.json';
+
+
+    #[Route('/classrooms', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        $classrooms = json_decode(
+            file_get_contents($this->file),
+            true
+        );
+
+        return new JsonResponse($classrooms);
+    }
+}
